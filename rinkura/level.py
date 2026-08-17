7ab4bb3fc3cd0267958183b1aa18e15ca17569d2
rokiri <rhadeasatwika21@gmail.com>
2026-08-17 17:27:06 +0800

    update test level

--- rinkura/level.py
+++ rinkura/level.py
@@ -1,20 +1,22 @@
 from sonolus.script.level import Level, LevelData
 
-from rinkura.play.mode import TapNote
+from rinkura.play.mode import FlickNote, TapNote
 from rinkura.play.stage import Stage
 
 level = Level(
-    name="rinkura_level",
-    title="Link! Like! Love Live! Level",
+    name="test_level",
+    title="Test Level",
     bgm=None,
     data=LevelData(
         bgm_offset=0,
         entities=[
             Stage(),
-            TapNote(beat=3, l1_raw=25, r1_raw=35),
-            TapNote(beat=4, l1_raw=15, r1_raw=25),
-            TapNote(beat=5, l1_raw=35, r1_raw=45),
             TapNote(beat=6, l1_raw=25, r1_raw=35),
+            TapNote(beat=7, l1_raw=15, r1_raw=25),
+            TapNote(beat=8, l1_raw=35, r1_raw=45),
+            TapNote(beat=9, l1_raw=25, r1_raw=35),
+            FlickNote(beat=10, l1_raw=25, r1_raw=35),
+            FlickNote(beat=11, l1_raw=15, r1_raw=25),
         ],
     ),
 )
